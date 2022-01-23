dfd51f1ccc3d8babdfeb695c2fb3f39c75bfa361
Pekka Ranta-aho <pekkaranta-aho@Pekkas-MacBook-Pro.local>
2022-01-23 22:55:36 +0200

    Created a method for initializing a new empty board

--- src/Board.mjs
+++ src/Board.mjs
@@ -1,13 +1,30 @@
 export class Board {
   width;
   height;
+  board = "";
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.initializeBoard();
   }
 
+  initializeBoard() {
+    for (let i = 0; i < this.height; i++) {
+      this.board = this.board.concat(".".repeat(this.width)+"\n");
+    }
+  }
+
+
+  /*
+  String representation of the board
+  */
   toString() {
-    return "...\n...\n...\n";
+    return this.board;
+  }
+
+  drop(block) {
+    //this.board.
+    this.board = ".X.\n...\n...\n";
   }
 }
