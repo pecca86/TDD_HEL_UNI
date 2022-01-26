56b4797bc4adddc38cb4c4f9142f6044fbdb1520
Pekka Ranta-aho <pekkaranta-aho@Pekkas-MacBook-Pro.local>
2022-01-26 22:31:35 +0200

    halfway implementing tick

--- src/Board.mjs
+++ src/Board.mjs
@@ -3,6 +3,9 @@ export class Board {
   height;
   board = "";
   testBoard = [];
+  isFalling = false;
+  block;
+  fallingBlockRow = 0;
 
   constructor(width, height) {
     this.width = width;
@@ -32,20 +35,38 @@ export class Board {
     /* let strrep = "";
     this.testBoard.forEach(item => strrep = strrep.concat(...item))
     return strrep; */
-    return this.testBoard.join("").toString();
-    //return this.board;
+    //return this.testBoard.join("").toString();
+    return this.board;
   }
+
+
+  // Returns true if there is a falling block
+  hasFalling() {
+    return this.isFalling;
+  }
+
+
+  // Checks if 
+  hasFallingAtRow() {
+    for (let row = 0; row < this.height; row++) {
+      for (let col = 0; col < this.width; col++) {
+        return this.hasFalling() && row == this.fallingBlockRow && col == 1;
+      }
+    }
+  }
+
 
   /* Drops the given block starting from the middle position of the board */
   drop(block) {
-    //this.board = `.${block}.\n...\n...\n`;
-    this.testBoard[0] = `.${block}.\n`;
+    this.block = block;
+    this.board = `.${this.block}.\n...\n...\n`;
+    //this.testBoard[0] = `.${block}.\n`;
+    this.isFalling = true;
   }
 
 
   /* Moves the block one level downwards */
   tick() {
-    //this.testBoard[0] = "wkodkwo"
     let tmp = this.testBoard[1];
     this.testBoard[1] = this.testBoard[0];
     this.testBoard[0] = tmp;
