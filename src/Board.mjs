a91223baf165073c44f4ddf6b72d827307e5d566
Pekka Ranta-aho <pekkaranta-aho@Pekkas-MacBook-Pro.local>
2022-01-29 23:26:29 +0200

    refactored

--- src/Board.mjs
+++ src/Board.mjs
@@ -1,8 +1,7 @@
 export class Board {
   width;
   height;
-  board = "";
-  testBoard = [];
+  gameBoard = [];
   isFalling = false;
   block;
   fallingBlockRow;
@@ -20,11 +19,7 @@ export class Board {
     let row = new Array(".".repeat(this.width) + "\n");
 
     for (let i = 0; i < this.height; i++) {
-      this.testBoard.push(row);
-    }
-
-    for (let i = 0; i < this.height; i++) {
-      this.board = this.board.concat(".".repeat(this.width) + "\n");
+      this.gameBoard.push(row);
     }
   }
 
@@ -33,27 +28,13 @@ export class Board {
   String representation of the board
   */
   toString() {
-    //let strrep = "";
-    //this.testBoard.forEach(item => strrep = strrep.concat(...item))
-    //return strrep;
-    return this.testBoard.join("").toString();
-    //return this.board;
+    return this.gameBoard.join("").toString();
   }
 
 
   // Returns true if there is a falling block
   hasFalling() {
     return this.isFalling;
-  }
-
-
-  // Checks if there is still a falling block at given row
-  hasFallingAtRow() {
-    for (let row = 0; row < this.height; row++) {
-      for (let col = 0; col < this.width; col++) {
-        return this.hasFalling() && row == this.fallingBlockRow && col == 1;
-      }
-    }
   }
 
 
@@ -66,61 +47,37 @@ export class Board {
       this.fallingBlockRow = 0;
       this.isFalling = true;
       this.block = block;
-      this.board = `.${this.block}.\n...\n...\n`;
-      this.testBoard[0] = `.${block}.\n`;
-      //this.fallingBlockRow++;
+      // TODO: Refactor: Needs to check if there is an existing block at row 0
+      // Remove hardcoded string
+      this.gameBoard[0] = `.${block}.\n`;
     }
   }
-
-
-
-  // TODO: SAVE THE BOARD STATE!
 
   /* Moves the block one level downwards */
   tick() {
-
     if (this.isFalling) {
-      // TODO: Check if row already consists of a previous block
-      // Possible solution: height - 1 
-/*       if (this.testBoard[this.fallingBlockRow] !== "...\n") {
-        //return;
-      } */
-
-      let tmp = this.testBoard[this.fallingBlockRow+1];
-      this.testBoard[this.fallingBlockRow+1] = this.testBoard[this.fallingBlockRow];
-      this.testBoard[this.fallingBlockRow] = tmp;
-      this.fallingBlockRow++;
-      
-      // Check if block reached to bottom
-      // Reduce height by one, since this row is now occupied
-      if (this.fallingBlockRow === this.height) {
-        this.isFalling = false;
-        this.height -= 1;
-      }
+      this.descendBlockOneRow();
+      this.checkIfBlockHitBottom();
     }
-
-
-
-
-    // Check if there is still a block falling
-    // If not, enable the block to tick down one row
-    /* if (this.fallingBlockRow == this.height) {
-      this.isFalling = false;
-    } else {
-      this.board = "";
-      for (let row = 0; row < this.width; row++) {
-        for (let col = 0; col < this.height; col++) {
-          if (col == 1 && row == this.fallingBlockRow) {
-            this.board = this.board.concat(this.block);
-          } else {
-            this.board = this.board.concat(".");
-          }
-        }
-        this.board = this.board.concat("\n");
-      }
-      this.fallingBlockRow++;
-    } */
   }
+
+  // Moves the current block down one row
+  descendBlockOneRow() {
+    let tmp = this.gameBoard[this.fallingBlockRow + 1];
+    this.gameBoard[this.fallingBlockRow + 1] = this.gameBoard[this.fallingBlockRow];
+    this.gameBoard[this.fallingBlockRow] = tmp;
+    this.fallingBlockRow++;
+  }
+
+  // Check if block reached to bottom
+  // Reduce height by one, since this row is now occupied
+  checkIfBlockHitBottom() {
+    if (this.fallingBlockRow === this.height) {
+      this.isFalling = false;
+      this.height -= 1;
+    }
+  }
+
 }
 
 
